eb838a617dd6be697aa42fe70220d60d110731fd
Miroslav Palka <palkamiroslav.v@gmail.com>
2022-07-04 12:10:40 +0100

    initialize repo

---
+++ helloworld.rb
@@ -0,0 +1,28 @@
+require 'sinatra'
+require 'sinatra/reloader' if development?
+
+get '/' do
+  output = "Hello world!"
+  output << "<br />"
+  output << "<a href=\"goodbye\">Leave</a>"
+end
+
+get '/goodbye' do
+  "Goodbye world!"
+end
+
+get '/about' do
+  output = "<h2>Sinatra</h2>"
+  output << "<p>This is my first Sinatra app.</p>"
+end
+
+get '/form' do
+  form = '<form action="/form" method="post">'
+  form << '<input type="submit" value="Submit" />'
+  form << '</form>'
+end
+
+post '/form' do
+  "Thank you for submitting the form."
+end
+
